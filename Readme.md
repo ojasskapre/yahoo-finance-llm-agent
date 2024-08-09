b10dad2c1a0244dfb80f4b543bd03497a267f986
ojasskapre <ojasskapre@gmail.com>
2024-08-08 23:52:56 -0700

    updated streamlit ui with sample questions and chat message history

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Resources
+
+- [Streamlit Agent Tools UI with Langchain](https://python.langchain.com/v0.2/docs/integrations/callbacks/streamlit/)
+- [Streamlit Chat Message History UI with Langchain](https://python.langchain.com/v0.2/docs/integrations/memory/streamlit_chat_message_history/)
+- [Streamlit Agent UI examples GitHub repository]()
--- app.py
+++ app.py
@@ -9,9 +9,13 @@ from langchain.agents import AgentExecutor
 from langchain_community.callbacks.streamlit import (
     StreamlitCallbackHandler,
 )
+from langchain.schema import ChatMessage
 
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key = os.environ['OPENAI_API_KEY']
+
+st.set_page_config(layout="wide")
+
 
 def register_tools():
     if not hasattr(tool_registry, "_is_registered"):
@@ -41,11 +45,77 @@ agent_chain = create_chain(tools)
 # Create the agent executor
 agent_executor = AgentExecutor(agent=agent_chain, tools=tools, verbose=True)
 
+st.title("Yahoo Finance Agent")
+
+st.markdown("""
+Welcome to the Finance Insights Assistant! This application leverages OpenAI, Langchain Agents and the Yahoo Finance Python library to provide you with up-to-date financial data and insights. Whether you're interested in stock prices, company financials, or the latest news, this assistant is here to help.
+
+##### Features:
+- **Stock Information**: Get detailed information about any stock, including historical data and recent news.
+- **Financial Statements**: Access income statements, balance sheets, and cash flow statements, both annual and quarterly.
+- **Options Data**: Explore available options expiration dates and detailed options chains.
+- **Holders and Recommendations**: View information about major holders, insider transactions, and analyst recommendations.
+""")
+
+# Sidebar for Sample Questions
+st.sidebar.markdown("### Sample Questions")
+sample_questions = {
+    "Stock Information": [
+        "What is the current price of Apple stock?",
+        "Give me the historical data for Tesla over the last month.",
+        "What are the latest news articles for Microsoft?"
+    ],
+    "Financial Statements": [
+        "Show me the latest income statement for Google.",
+        "What is the quarterly cash flow for Amazon?",
+        "Can you provide the annual balance sheet for Facebook?"
+    ],
+    "Options Data": [
+        "What are the available options expiration dates for Apple?",
+        "Show me the options chain for Tesla expiring on 2024-01-19."
+    ],
+    "Holders and Recommendations": [
+        "Who are the major holders of Netflix?",
+        "What are the recent analyst recommendations for Tesla?",
+        "Show me the recent insider transactions for Microsoft."
+    ]
+}
+
+# Display Sample Questions in the Sidebar with Expanders
+for category, questions in sample_questions.items():
+    with st.sidebar.expander(f"{category}"):
+        for question in questions:
+            st.sidebar.markdown(f"- {question}")
+
+# TODO: Accept OpenAI API key as input in sidebar
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [ChatMessage(role="assistant", content="How can I help you?")]
+
+# Display the chat messages already in the session state
+for msg in st.session_state.messages:
+    st.chat_message(msg.role).write(msg.content)
+
 if prompt := st.chat_input():
+    # add the user message to the session state
+    st.session_state.messages.append(ChatMessage(role="user", content=prompt))
+    
+    # display the user message in the chat
     st.chat_message("user").write(prompt)
+
     with st.chat_message("assistant"):
-        st_callback = StreamlitCallbackHandler(st.container())
-        response = agent_executor.invoke(
-            {"input": prompt}, {"callbacks": [st_callback]}
-        )
-        st.write(response["output"])
+        # Uncomment the following lines to enable Streamlit callbacks and display agent execution
+        
+        # st_callback = StreamlitCallbackHandler(st.container())
+        # response = agent_executor.invoke(
+        #     {"input": prompt}, {"callbacks": [st_callback]}
+        # )
+        try: 
+            response = agent_executor.invoke(
+                {"input": prompt}
+            )
+            st.write(response["output"])
+            st.session_state.messages.append(ChatMessage(role="assistant", content=response["output"]))
+        except Exception as e:
+            st.write(f"An error occurred: {e}")
+            st.session_state.messages.append(ChatMessage(role="assistant", content="An error occurred. Please try again."))
